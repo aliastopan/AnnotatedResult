36d6baf6b1d0b1255555b7e96805f8b383acf81c
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-24 21:09:16 +0700

    add static error message references

---
+++ src/AnnotatedResult/Common/ErrorMessage.cs
@@ -0,0 +1,10 @@
+namespace AnnotatedResult.Common
+{
+    internal static class ErrorMessage
+    {
+        internal const string Unauthorized = "Your request has been denied.";
+        internal const string Forbidden = "You don't have permission to access this resource.";
+        internal const string Conflict = "Your request cannot be processed.";
+        internal const string NotFound = "Resource not found.";
+    }
+}
