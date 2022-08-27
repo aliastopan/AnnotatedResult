bef1bcb2a558b90bf8a0f19dfad54b3943b27f37
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-28 05:41:39 +0700

    fix: duplicate error messages

--- src/AnnotatedResult/InstanceExtensions.cs
+++ src/AnnotatedResult/InstanceExtensions.cs
@@ -4,10 +4,9 @@ namespace AnnotatedResult
 {
     public static class InstanceExtensions
     {
-        private readonly static InternalValidator validator = new InternalValidator();
-
         public static bool TryValidate<T>(this T instance, out Error[] errors)
         {
+            var validator = new InternalValidator();
             return validator.TryValidate(instance, out errors);
         }
     }
