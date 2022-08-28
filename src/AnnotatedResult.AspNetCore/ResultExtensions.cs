5343232765517b9e6e3f3dcb69cfb21b60789fe4
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-28 19:33:44 +0700

    add more 'Result' overloading

--- src/AnnotatedResult.AspNetCore/ResultExtensions.cs
+++ src/AnnotatedResult.AspNetCore/ResultExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,94 @@ public static class ResultExtensions
     public static IResult HttpResult<T>(this Result<T> result)
     {
         return Match(result);
+    }
+
+    public static IResult HttpResult(this Result result, ProblemDetails details)
+    {
+        return result.Status switch
+        {
+            ResultStatus.Ok => Results.Ok(),
+            _ => result.Problem(details)
+        };
+    }
+
+    public static IResult HttpResult(this Result result, HttpContext context)
+    {
+        return result.Status switch
+        {
+            ResultStatus.Ok => Results.Ok(),
+            _ => result.Problem(context)
+        };
+    }
+
+    public static IResult HttpResult<T>(this Result<T> result, HttpContext context)
+    {
+        return result.Status switch
+        {
+            ResultStatus.Ok => Results.Ok(result.Value),
+            _ => result.Problem(context)
+        };
+    }
+
+    public static IResult HttpResult<T>(this Result<T> result, ProblemDetails details)
+    {
+        return result.Status switch
+        {
+            ResultStatus.Ok => Results.Ok(result.Value),
+            _ => result.Problem(details)
+        };
+    }
+
+    public static IResult HttpResult<T>(this Result<T> result, HttpContext context, ProblemDetails details)
+    {
+        return result.Status switch
+        {
+            ResultStatus.Ok => Results.Ok(result.Value),
+            _ => result.Problem(context, details)
+        };
+    }
+
+    private static IResult Problem(this Result result, HttpContext context)
+    {
+        var errors = new List<string>();
+        foreach(var error in result.Errors)
+        {
+            errors.Add(error.Message);
+        }
+
+        var details = new ProblemDetails
+        {
+            Status = (int)result.Status,
+            Instance = context.Request.Path
+        };
+        details.Extensions["traceId"] = Activity.Current?.Id ?? context.TraceIdentifier;
+        return Results.Problem(details);
+    }
+
+    private static IResult Problem(this Result result, ProblemDetails details)
+    {
+        var errors = new List<string>();
+        foreach (var error in result.Errors)
+        {
+            errors.Add(error.Message);
+        }
+        details.Status = (int)result.Status;
+        details.Extensions["errors"] = errors;
+        return Results.Problem(details);
+    }
+
+    private static IResult Problem(this Result result, HttpContext context, ProblemDetails details)
+    {
+        var errors = new List<string>();
+        foreach (var error in result.Errors)
+        {
+            errors.Add(error.Message);
+        }
+        details.Status = (int)result.Status;
+        details.Instance = context.Request.Path;
+        details.Extensions["traceId"] = Activity.Current?.Id ?? context.TraceIdentifier;
+        details.Extensions["errors"] = errors;
+        return Results.Problem(details);
     }
 
     private static IResult Match(Result result)
