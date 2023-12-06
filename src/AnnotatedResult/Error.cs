d948f8421ddc5a08d6bea3510e7ab83c6c34c994
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-06 13:22:38 +0700

    add summary documentation

--- src/AnnotatedResult/Error.cs
+++ src/AnnotatedResult/Error.cs
@@ -1,11 +1,26 @@
 namespace AnnotatedResult
 {
+    /// <summary>
+    /// Represents an error with a specific message and severity level.
+    /// </summary>
     public struct Error
     {
+        /// <summary>
+        /// Gets the error message.
+        /// </summary>
         public string Message { get; }
+
+        /// <summary>
+        /// Gets or sets the severity level of the error.
+        /// </summary>
         public ErrorSeverity Severity { get; set; }
 
-        public Error(string message, ErrorSeverity severity = ErrorSeverity.Error)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Error"/> struct with the specified message and optional severity.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="severity">The severity level of the error (default is <see cref="ErrorSeverity.Information"/>).</param>
+        public Error(string message, ErrorSeverity severity = ErrorSeverity.Information)
         {
             Message = message;
             Severity = severity;
