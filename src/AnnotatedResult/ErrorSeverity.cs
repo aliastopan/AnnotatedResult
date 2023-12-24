ed2b9f8ba4257602dfe6233958d06b96bcf046a7
Taufan Augusta <taufan.augusta@protonmail.com>
2023-12-25 02:02:12 +0700

    fix: styling

--- src/AnnotatedResult/ErrorSeverity.cs
+++ src/AnnotatedResult/ErrorSeverity.cs
@@ -1,43 +1,43 @@
 namespace AnnotatedResult
 {
-/// <summary>
-/// Represents the severity levels of errors.
-/// </summary>
-public enum ErrorSeverity
-{
     /// <summary>
-    /// Informational messages that do not indicate an issue.
+    /// Represents the severity levels of errors.
     /// </summary>
-    Information,
+    public enum ErrorSeverity
+    {
+        /// <summary>
+        /// Informational messages that do not indicate an issue.
+        /// </summary>
+        Information,
 
-    /// <summary>
-    /// Messages used during debugging and development to provide detailed information for troubleshooting.
-    /// </summary>
-    Debug,
+        /// <summary>
+        /// Messages used during debugging and development to provide detailed information for troubleshooting.
+        /// </summary>
+        Debug,
 
-    /// <summary>
-    /// Events that are noteworthy but may not require immediate attention.
-    /// </summary>
-    Notice,
+        /// <summary>
+        /// Events that are noteworthy but may not require immediate attention.
+        /// </summary>
+        Notice,
 
-    /// <summary>
-    /// Indicates a potential problem or anomaly that doesn't necessarily prevent the system from functioning.
-    /// </summary>
-    Warning,
+        /// <summary>
+        /// Indicates a potential problem or anomaly that doesn't necessarily prevent the system from functioning.
+        /// </summary>
+        Warning,
 
-    /// <summary>
-    /// Represents a critical issue that prevents the normal functioning of the system or application.
-    /// </summary>
-    Error,
+        /// <summary>
+        /// Represents a critical issue that prevents the normal functioning of the system or application.
+        /// </summary>
+        Error,
 
-    /// <summary>
-    /// Similar to "Error," indicating a severe problem that requires immediate attention.
-    /// </summary>
-    Critical,
+        /// <summary>
+        /// Similar to "Error," indicating a severe problem that requires immediate attention.
+        /// </summary>
+        Critical,
 
-    /// <summary>
-    /// Reserved for the most severe issues that require immediate attention and may result in a system failure.
-    /// </summary>
-    Emergency
-}
+        /// <summary>
+        /// Reserved for the most severe issues that require immediate attention and may result in a system failure.
+        /// </summary>
+        Emergency
+    }
 }
