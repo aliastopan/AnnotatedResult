0e49e912e8dbc35105ea608e3319600fb865f7d4
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-24 18:34:14 +0700

    add docs

--- src/AnnotatedResult/InstanceExtensions.cs
+++ src/AnnotatedResult/InstanceExtensions.cs
@@ -2,14 +2,32 @@ using AnnotatedResult.Internal;
 
 namespace AnnotatedResult
 {
+    /// <summary>
+    /// Provides extension methods for validating instances.
+    /// </summary>
     public static class InstanceExtensions
     {
+        /// <summary>
+        /// Attempts to validate the specified instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to validate.</typeparam>
+        /// <param name="instance">The instance to validate.</param>
+        /// <param name="errors">When this method returns, contains any errors that occurred during validation.</param>
+        /// <returns><c>true</c> if the instance is valid; otherwise, <c>false</c>.</returns>
         public static bool TryValidate<T>(this T instance, out Error[] errors)
         {
             var validator = new InternalValidator();
             return validator.TryValidate(instance, out errors);
         }
 
+        /// <summary>
+        /// Attempts to validate the specified instance using the provided validator.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to validate.</typeparam>
+        /// <param name="instance">The instance to validate.</param>
+        /// <param name="validator">The validator to use.</param>
+        /// <param name="errors">When this method returns, contains any errors that occurred during validation.</param>
+        /// <returns><c>true</c> if the instance is valid; otherwise, <c>false</c>.</returns>
         public static bool TryValidate<T>(this T instance, IResultValidator validator, out Error[] errors)
         {
             return validator.TryValidate(instance, out errors);
